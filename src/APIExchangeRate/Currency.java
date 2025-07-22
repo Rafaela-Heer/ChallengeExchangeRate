94daf9b63f85b670a1484c13ae31aacb3e002b15
Rafaela-Heer <rafaelaheer@hotmail.com>
2025-07-22 15:30:03 -0300

    Update menu

--- src/APIExchangeRate/Currency.java
+++ src/APIExchangeRate/Currency.java
@@ -7,6 +7,7 @@ public class Currency {
     private double conversion_rate;
     private double conversion_result;
 
+
     public String getResult() {
         return result;
     }
@@ -25,5 +26,171 @@ public class Currency {
 
     public double getConversion_result() {
         return conversion_result;
+    }
+
+    public static String getRates() {
+        String rates = "\nUse the currency code:\n" +
+                "AED\tUAE Dirham\n" +
+                "AFN\tAfghan Afghani\n" +
+                "ALL\tAlbanian Lek\n" +
+                "AMD\tArmenian Dram\n" +
+                "ANG\tNetherlands Antillian Guilder\n" +
+                "AOA\tAngolan Kwanza\n" +
+                "ARS\tArgentine Peso\n" +
+                "AUD\tAustralian Dollar\n" +
+                "AWG\tAruban Florin\n" +
+                "AZN\tAzerbaijani Manat\n" +
+                "BAM\tBosnia and Herzegovina Mark\n" +
+                "BBD\tBarbados Dollar\n" +
+                "BDT\tBangladeshi Taka\n" +
+                "BGN\tBulgarian Lev\n" +
+                "BHD\tBahraini Dinar\n" +
+                "BIF\tBurundian Franc\n" +
+                "BMD\tBermudian Dollar\n" +
+                "BND\tBrunei Dollar\n" +
+                "BOB\tBolivian Boliviano\n" +
+                "BRL\tBrazilian Real\n" +
+                "BSD\tBahamian Dollar\n" +
+                "BTN\tBhutanese Ngultrum\n" +
+                "BWP\tBotswana Pula\n" +
+                "BYN\tBelarusian Ruble\n" +
+                "BZD\tBelize Dollar\n" +
+                "CAD\tCanadian Dollar\n" +
+                "CDF\tCongolese Franc\n" +
+                "CHF\tSwiss Franc\n" +
+                "CLP\tChilean Peso\n" +
+                "CNY\tChinese Renminbi\n" +
+                "COP\tColombian Peso\n" +
+                "CRC\tCosta Rican Colon\n" +
+                "CUP\tCuban Peso\n" +
+                "CVE\tCape Verdean Escudo\n" +
+                "CZK\tCzech Koruna\n" +
+                "DJF\tDjiboutian Franc\n" +
+                "DKK\tDanish Krone\n" +
+                "DOP\tDominican Peso\n" +
+                "DZD\tAlgerian Dinar\n" +
+                "EGP\tEgyptian Pound\n" +
+                "ERN\tEritrean Nakfa\n" +
+                "ETB\tEthiopian Birr\n" +
+                "EUR\tEuro\n" +
+                "FJD\tFiji Dollar\n" +
+                "FKP\tFalkland Islands Pound\n" +
+                "FOK\tFaroese Króna\n" +
+                "GBP\tPound Sterling\n" +
+                "GEL\tGeorgian Lari\n" +
+                "GGP\tGuernsey Pound\n" +
+                "GHS\tGhanaian Cedi\n" +
+                "GIP\tGibraltar Pound\n" +
+                "GMD\tGambian Dalasi\n" +
+                "GNF\tGuinean Franc\n" +
+                "GTQ\tGuatemalan Quetzal\n" +
+                "GYD\tGuyanese Dollar\n" +
+                "HKD\tHong Kong Dollar\n" +
+                "HNL\tHonduran Lempira\n" +
+                "HRK\tCroatian Kuna\n" +
+                "HTG\tHaitian Gourde\n" +
+                "HUF\tHungarian Forint\n" +
+                "IDR\tIndonesian Rupiah\n" +
+                "ILS\tIsraeli New Shekel\n" +
+                "IMP\tManx Pound\n" +
+                "INR\tIndian Rupee\n" +
+                "IQD\tIraqi Dinar\n" +
+                "IRR\tIranian Rial\n" +
+                "ISK\tIcelandic Króna\n" +
+                "JEP\tJersey Pound\n" +
+                "JMD\tJamaican Dollar\n" +
+                "JOD\tJordanian Dinar\n" +
+                "JPY\tJapanese Yen\n" +
+                "KES\tKenyan Shilling\n" +
+                "KGS\tKyrgyzstani Som\n" +
+                "KHR\tCambodian Riel\n" +
+                "KID\tKiribati Dollar\n" +
+                "KMF\tComorian Franc\n" +
+                "KRW\tSouth Korean Won\n" +
+                "KWD\tKuwaiti Dinar\n" +
+                "KYD\tCayman Islands Dollar\n" +
+                "KZT\tKazakhstani Tenge\n" +
+                "LAK\tLao Kip\n" +
+                "LBP\tLebanese Pound\n" +
+                "LKR\tSri Lanka Rupee\n" +
+                "LRD\tLiberian Dollar\n" +
+                "LSL\tLesotho Loti\n" +
+                "LYD\tLibyan Dinar\n" +
+                "MAD\tMoroccan Dirham\n" +
+                "MDL\tMoldovan Leu\n" +
+                "MGA\tMalagasy Ariary\n" +
+                "MKD\tMacedonian Denar\n" +
+                "MMK\tBurmese Kyat\n" +
+                "MNT\tMongolian Tögrög\n" +
+                "MOP\tMacanese Pataca\n" +
+                "MRU\tMauritanian Ouguiya\n" +
+                "MUR\tMauritian Rupee\n" +
+                "MVR\tMaldivian Rufiyaa\n" +
+                "MWK\tMalawian Kwacha\n" +
+                "MXN\tMexican Peso\n" +
+                "MYR\tMalaysian Ringgit\n" +
+                "MZN\tMozambican Metical\n" +
+                "NAD\tNamibian Dollar\n" +
+                "NGN\tNigerian Naira\n" +
+                "NIO\tNicaraguan Córdoba\n" +
+                "NOK\tNorwegian Krone\n" +
+                "NPR\tNepalese Rupee\n" +
+                "NZD\tNew Zealand Dollar\n" +
+                "OMR\tOmani Rial\n" +
+                "PAB\tPanamanian Balboa\n" +
+                "PEN\tPeruvian Sol\n" +
+                "PGK\tPapua New Guinean Kina\n" +
+                "PHP\tPhilippine Peso\n" +
+                "PKR\tPakistani Rupee\n" +
+                "PLN\tPolish Złoty\n" +
+                "PYG\tParaguayan Guaraní\n" +
+                "QAR\tQatari Riyal\n" +
+                "RON\tRomanian Leu\n" +
+                "RSD\tSerbian Dinar\n" +
+                "RUB\tRussian Ruble\n" +
+                "RWF\tRwandan Franc\n" +
+                "SAR\tSaudi Riyal\n" +
+                "SBD\tSolomon Islands Dollar\n" +
+                "SCR\tSeychellois Rupee\n" +
+                "SDG\tSudanese Pound\n" +
+                "SEK\tSwedish Krona\n" +
+                "SGD\tSingapore Dollar\n" +
+                "SHP\tSaint Helena Pound\n" +
+                "SLE\tSierra Leonean Leone\n" +
+                "SOS\tSomali Shilling\n" +
+                "SRD\tSurinamese Dollar\n" +
+                "SSP\tSouth Sudanese Pound\n" +
+                "STN\tSão Tomé and Príncipe Dobra\n" +
+                "SYP\tSyrian Pound\n" +
+                "SZL\tEswatini Lilangeni\n" +
+                "THB\tThai Baht\n" +
+                "TJS\tTajikistani Somoni\n" +
+                "TMT\tTurkmenistan Manat\n" +
+                "TND\tTunisian Dinar\n" +
+                "TOP\tTongan Paʻanga\n" +
+                "TRY\tTurkish Lira\n" +
+                "TTD\tTrinidad and Tobago Dollar\n" +
+                "TVD\tTuvaluan Dollar\n" +
+                "TWD\tNew Taiwan Dollar\n" +
+                "TZS\tTanzanian Shilling\n" +
+                "UAH\tUkrainian Hryvnia\n" +
+                "UGX\tUgandan Shilling\n" +
+                "USD\tUnited States Dollar\n" +
+                "UYU\tUruguayan Peso\n" +
+                "UZS\tUzbekistani So'm\n" +
+                "VES\tVenezuelan Bolívar Soberano\n" +
+                "VND\tVietnamese Đồng\n" +
+                "VUV\tVanuatu Vatu\n" +
+                "WST\tSamoan Tālā\n" +
+                "XAF\tCentral African CFA Franc\n" +
+                "XCD\tEast Caribbean Dollar\n" +
+                "XDR\tSpecial Drawing Rights\n" +
+                "XOF\tWest African CFA franc\n" +
+                "XPF\tCFP Franc\n" +
+                "YER\tYemeni Rial\n" +
+                "ZAR\tSouth African Rand\n" +
+                "ZMW\tZambian Kwacha\n" +
+                "ZWL\tZimbabwean Dollar";
+        return rates;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,95 @@
 import APIExchangeRate.RequestApi;
 import APIExchangeRate.Currency;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
+        List<String> save = new ArrayList<>();
+        boolean running = true;
         Scanner read = new Scanner(System.in);
 
-        System.out.print("Moeda base \n-> ");
-        String inputCurrency = read.nextLine();
+        while (running) {
+            System.out.println("\nEXCHANGE RATE\n**********************************\n");
+            System.out.print("Choose one of the options\n " +
+                    "(1) Currency Conversion\n " +
+                    "(2) My conversion history\n " +
+                    "(3) Exit\n->");
+            int menu = read.nextInt();
+            read.nextLine();
 
-        System.out.print("Quanto você deseja trocar? \n-> ");
-        double currency = read.nextDouble();
-        read.nextLine();
+            switch (menu){
+                case 1:
+                    System.out.print("\n Want to see the currency code list? (1) Yes (2) No\n-> ");
+                    int codes = read.nextInt();
+                    read.nextLine();
+                    if (codes == 1) {
+                        System.out.println(Currency.getRates() + "\n\n");
+                    }
+                    System.out.print("\nFrom \n-> ");
+                    String inputCurrency = read.nextLine();
 
-        System.out.print("Moeda pra troca \n-> ");
-        String outputCurrency = read.nextLine();
 
-        Currency result = RequestApi.getConversion(inputCurrency, outputCurrency, currency);
+                    System.out.print("Amount \n-> ");
+                    double currency = read.nextDouble();
+                    read.nextLine();
 
-        if ("success".equalsIgnoreCase(result.getResult())) {
-            System.out.println("Conversão realizada com sucesso!");
-            System.out.println("De: " + result.getBase_code());
-            System.out.println("Para: " + result.getTarget_code());
-            System.out.println("Taxa de câmbio: " + result.getConversion_rate());
-            System.out.println("Valor convertido: " + result.getConversion_result());
-        } else {
-            System.out.println("ERRO");
+                    System.out.print("To \n-> ");
+                    String outputCurrency = read.nextLine();
+
+                    Currency result = RequestApi.getConversion(inputCurrency, outputCurrency, currency);
+
+                    if ("success".equalsIgnoreCase(result.getResult())) {
+                        System.out.println("\nConversion performed correctly!");
+                        System.out.println("From: " + result.getBase_code());
+                        System.out.println("To: " + result.getTarget_code());
+                        System.out.println("Exchange rate(now): " + result.getConversion_rate());
+                        System.out.println("Conversion " + result.getConversion_result());
+
+                        save.add(result.getBase_code() + " → " + result.getTarget_code()
+                                + " | Conversion: " + result.getConversion_result()
+                                + " | Rate: " + result.getConversion_rate());
+                    } else {
+                        System.out.println("ERROR");
+                    }
+                    System.out.print("\nDo you want to go back to the menu?\n (1)Yes\n (2)No\n-> ");
+                    int backMenu = read.nextInt();
+                    if (backMenu == 2) {
+                        running = false;
+                        System.out.println("\n...\nClosing\n...");
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("\nConversion history:");
+                    if (save.isEmpty()) {
+                        System.out.println("No conversions performed yet.");
+                    } else {
+                        for (String item : save) {
+                            System.out.println(item);
+                        }
+                    }
+                    System.out.print("\nDo you want to go back to the menu?\n (1)Yes\n (2)No\n-> ");
+                    int backMenu2 = read.nextInt();
+                    if (backMenu2 == 2) {
+                        running = false;
+                        System.out.println("\n...\nClosing\n...");
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("\n...\nClosing\n...");
+                    running = false;
+                    break;
+
+                default:
+                    System.out.println("ERROR");
+                    break;
+            }
         }
     }
 }
