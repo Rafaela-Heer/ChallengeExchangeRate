141797e91b49f42b95087e5a578f981c73700414
Rafaela-Heer <rafaelaheer@hotmail.com>
2025-07-24 15:02:31 -0300

    fix/ exceptions

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import APIExchangeRate.RequestApi;
 import APIExchangeRate.Currency;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 
 import java.io.IOException;
@@ -15,55 +16,112 @@ public class Main {
 
         while (running) {
             System.out.println("\nEXCHANGE RATE\n**********************************\n");
-            System.out.print("Choose one of the options\n " +
-                    "(1) Currency Conversion\n " +
-                    "(2) My conversion history\n " +
-                    "(3) Exit\n->");
-            int menu = read.nextInt();
-            read.nextLine();
-
-            switch (menu){
-                case 1:
-                    System.out.print("\n Want to see the currency code list?\n (1) Yes\n (2) No\n-> ");
-                    int codes = read.nextInt();
+            int menu = 0;
+            boolean validInput = false;
+            while (!validInput) {
+                try {
+                    System.out.print("Choose one of the options\n" +
+                            "(1) Currency Conversion\n" +
+                            "(2) My conversion history\n" +
+                            "(3) Exit\n-> ");
+                    menu = read.nextInt();
                     read.nextLine();
-                    if (codes == 1) {
-                        System.out.println(Currency.getRates() + "\n\n");
+                    validInput = true;
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid input. Please enter a number.");
+                    read.nextLine();
+                }
+            }
+            validInput = false;
+
+
+            switch (menu) {
+                case 1:
+                    int codes;
+                    while (!validInput) {
+                        try {
+                            do {
+                                System.out.print("\nWant to see the currency code list?\n (1) Yes\n (2) No\n-> ");
+                                codes = read.nextInt();
+                                read.nextLine();
+
+                                if (codes == 1) {
+                                    System.out.println(Currency.getRates() + "\n\n");
+                                } else if (codes != 2) {
+                                    System.out.println("\n ERROR \n Choose again");
+                                }
+                            } while (codes != 1 && codes != 2);
+                            validInput = true;
+                        } catch (InputMismatchException e) {
+                            System.out.println("\n Invalid input. Please enter a number.");
+                            read.nextLine();
+                        }
                     }
+                    validInput = false;
+
                     System.out.print("\nFrom: \n-> ");
                     String inputCurrency = read.nextLine();
 
 
-                    System.out.print("Amount: \n-> ");
-                    double currency = read.nextDouble();
-                    read.nextLine();
+                    Currency result = null;
+                    double currency = 0;
+                    while (!validInput) {
+                        try {
+                            System.out.print("Amount: \n-> ");
+                            currency = read.nextDouble();
+                            read.nextLine();
+                            validInput = true;
+                        } catch (InputMismatchException e) {
+                            System.out.println("\n Invalid input. Please enter a number.");
+                            read.nextLine();
+                        }
+                    }
 
                     System.out.print("To: \n-> ");
                     String outputCurrency = read.nextLine();
 
-                    Currency result = RequestApi.getConversion(inputCurrency, outputCurrency, currency);
 
-                    if ("success".equalsIgnoreCase(result.getResult())) {
-                        System.out.println("\nConversion performed correctly!");
-                        System.out.println("From: " + result.getBase_code());
-                        System.out.println("To: " + result.getTarget_code());
-                        System.out.println("Exchange rate(now): " + result.getConversion_rate());
-                        System.out.println("Conversion: " + result.getConversion_result());
+                    try {
+                        result = RequestApi.getConversion(inputCurrency, outputCurrency, currency);
 
-                        save.add(result.getBase_code() + " → " + result.getTarget_code()
-                                + " | Conversion: " + result.getConversion_result()
-                                + " | Rate: " + result.getConversion_rate());
-                    } else {
-                        System.out.println("ERROR");
+
+                        if ("success".equalsIgnoreCase(result.getResult())) {
+                            System.out.println("\nConversion performed correctly!");
+                            System.out.println("From: " + result.getBase_code());
+                            System.out.println("To: " + result.getTarget_code());
+                            System.out.println("Exchange rate(now): " + result.getConversion_rate());
+                            System.out.println("Conversion: " + result.getConversion_result());
+
+                            save.add(result.getBase_code() + " → " + result.getTarget_code()
+                                    + " | Conversion: " + result.getConversion_result()
+                                    + " | Rate: " + result.getConversion_rate());
+                        } else {
+                            System.out.println("ERROR");
+                        }
+
+                    } catch (IOException | InterruptedException e) {
+                        System.out.println(" Error connecting to the API: " + e.getMessage());
+                    } catch (Exception e) {
+                        System.out.println(" Error during conversion.");
                     }
-                    System.out.print("\nDo you want to go back to the menu?\n (1)Yes\n (2)No\n-> ");
-                    int backMenu = read.nextInt();
-                    if (backMenu == 2) {
-                        running = false;
-                        System.out.println("\n...\nCLOSING\n...");
+
+                    validInput = false;
+                    while (!validInput) {
+                        try {
+                            System.out.print("\nDo you want to go back to the menu?\n (1)Yes\n (2)No\n-> ");
+                            int backMenu = read.nextInt();
+                            if (backMenu == 2) {
+                                running = false;
+                                System.out.println("\n...\nCLOSING\n...");
+                            }
+                            validInput = true;
+                        } catch (InputMismatchException e) {
+                            System.out.println("\n Invalid input. Please enter a number.");
+                            read.nextLine();
+                        }
                     }
+
                     break;
-
                 case 2:
                     System.out.println("\nConversion history:");
                     if (save.isEmpty()) {
@@ -72,12 +130,23 @@ public class Main {
                         for (String item : save) {
                             System.out.println(item);
                         }
-                    }
-                    System.out.print("\nDo you want to go back to the menu?\n (1)Yes\n (2)No\n-> ");
-                    int backMenu2 = read.nextInt();
-                    if (backMenu2 == 2) {
-                        running = false;
-                        System.out.println("\n...\nCLOSING\n...");
+
+
+                        while (!validInput) {
+                            try {
+                                System.out.print("\nDo you want to go back to the menu?\n (1)Yes\n (2)No\n-> ");
+                                int backMenu2 = read.nextInt();
+                                if (backMenu2 == 2) {
+                                    running = false;
+                                    System.out.println("\n...\nCLOSING\n...");
+                                }
+                                validInput = true;
+                            } catch (InputMismatchException e) {
+                                System.out.println("\n Invalid input. Please enter a number.");
+                                read.nextLine();
+                            }
+                        }
+
                     }
                     break;
 
@@ -86,8 +155,9 @@ public class Main {
                     running = false;
                     break;
 
+
                 default:
-                    System.out.println("ERROR");
+                    System.out.println("\n ERROR \n Choose again");
                     break;
             }
         }
